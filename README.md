c8d2f80788f52ce3bc9890dd3b412d38356c8acd
Sermed92 <sermed19@gmail.com>
2016-10-08 12:10:00 -0400

    Configurada lectura de argumentos, leido archivo y datos guardados en matrices

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # batalla
+
+-Implementada la lectura de parámetros con getopt
+-Leídos los datos del archivo y guardados en las matrices 'campoB' y 'bombas'
+
+Nota: Por ahora los estoy guardando en matrices, pero quizás podríamos guardar las bombas en una cola
---
+++ main.c
@@ -0,0 +1,122 @@
+/*
+ * Archivo: main.c
+ * Autores:
+ *          RECUERDA COLOCARTE
+ *          Sergio Medina 09-11259
+ * Descripción:   
+ */
+
+/* Librerias */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
+
+// Finaliza el programa si no posee la cantidad de argumentos necesarios, despues de indicar la forma correcta
+ void numeroArgumentos(int cantidad){
+    if ((cantidad < 2) || (cantidad > 5)){
+        printf("Violacion de parametros, pruebe: \n");
+        printf("./batalla [-p] [-h] [-n <cantidad>] <archivo_entrada> \n");
+        printf("Notese que los parametros entre [] son opcionales\n");
+        exit(1);
+    }
+ }
+
+ int main (int argc, char** argv){
+
+    numeroArgumentos(argc);
+
+    int hflag = 0;
+    int pflag = 0;
+    int nvalue = 0;
+    int n;
+
+    opterr = 0;
+
+    while ((n = getopt(argc, argv, "hpn:")) != -1){
+        switch(n){
+            case 'h':
+            {
+                hflag = 1;
+                break;
+            }
+            case 'p':
+            {
+                pflag = 1;
+                break;
+            }
+            case 'n':
+            {
+                nvalue = optarg - '0';
+                break;
+            }
+            case '?':
+            {
+                if (optopt == 'n'){
+                    fprintf(stderr, "Option -%c requiere un argumento. \n",optopt);
+                } else if (isprint(optopt)){
+                    fprintf(stderr, "Opcion desconocida '-%c'.\n", optopt);
+                } else{
+                    fprintf(stderr, "Caracter desconocido '\\x%x'.\n", optopt);
+                }
+                return 1;
+            }
+            default:
+            {
+                abort();
+            }
+        }
+    }
+
+    if ((hflag==1) && (pflag==1)){
+        printf("-h y -p son opciones excluyentes\n");
+        exit(1);
+    } else if (hflag==1) {
+        //Se trabajara con hilos
+    } else {
+        // p opcion por defecto, no entrara si se da el argumento de hilo
+        //Se trabajara con procesos
+    }
+
+    // Seccion de lectura del archivo
+    FILE *archivo = NULL;
+    archivo = fopen(argv[optind], "r");
+
+    if (archivo == NULL) {
+        printf(" ERROR: Archivo no encontrado \n");
+        exit(1);
+    }
+
+    int tamanio, objetivos, numBombas, cord1, cord2, valor;
+
+    fscanf(archivo,"%d",tamanio);
+
+    int campoB[tamanio][tamanio];
+
+    fscanf(archivo,"%d",objetivos);
+
+    // Se guardan los objetivos en la matriz
+    for (int i = 0; i<objetivos, i++){
+        fscanf(archivo,"%d %d %d", &cord1, &cord2, &valor);
+        campoB[cord1][cord2] = valor;
+    }
+
+    fscanf(archivo,"%d",numBombas);
+
+    int bombas[numBombas][4];
+    int radio, potencia;
+
+    //Se guardan las bombas que serán usadas
+    for (int i = 0; i<numBombas, i++){
+        fscanf(archivo,"%d %d %d %d", &cord1, &cord2, &radio, &potencia);
+        bombas[i][0] = cord1;
+        bombas[i][1] = cord2;
+        bombas[i][2] = radio;
+        bombas[i][3] = potencia;
+    }
+
+    fclose(archivo);
+    
+    return 0;
+
+ }
